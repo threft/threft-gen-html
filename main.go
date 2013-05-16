87524b6cdc11a03c8e35917c8482ec939df3aaf9
GeertJohan <gjr19912@gmail.com>
2013-05-17 00:06:41 +0200

    Receiving tidm-json and dumping to stdout.

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/threft/threft/tidm"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello. This is threft-gen-html")
+	t, err := tidm.ReadFrom(os.Stdin)
+	if err != nil {
+		fmt.Printf("Error reading TIDM from stdin: %s\n", err)
+		return
+	}
+	spew.Dump(t)
 }
